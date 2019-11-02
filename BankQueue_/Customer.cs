c2b2273db568a03b38a28be643e74a565468a77f
Kahraman Bayraktar <kahraman.bayraktar@ite-turkey.com>
2019-11-02 18:25:50 +0300

    Removed unused directory.

--- BankQueue_/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BankQueue
-{
-    public class Customer
-    {
-        public int CustomerId { get; set; }
-        public string CustomerName { get; set; }
-        public int Priority { get; set; }
-    }
-}
